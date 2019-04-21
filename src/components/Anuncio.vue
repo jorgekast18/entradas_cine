67a1cde32e52d3c8c3fc5d8ecc8e5dbe98ae98d6
Familia Castano Rodriguez <familiacastano@iMac-de-Familia.local>
2019-04-21 15:27:28 -0500

    Modify logic for animation

--- src/components/Anuncio.vue
+++ src/components/Anuncio.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="anuncio">
-        <span>{{ presentations[0] }}</span>
+        <transition name="fade" mode="out-in">
+            <span :key="index">{{ presentations[index] }}</span>
+        </transition>
     </div>
 </template>
 
@@ -17,6 +19,19 @@ export default {
             index: 0
         }
     },
+    methods: {
+        showAnnounce() {
+            this.index++
+
+            if(this.index === this.presentations.length) {
+                this.index = 0
+            }
+
+        }
+    },
+    mounted() {
+        setInterval(this.showAnnounce, 3000)
+    }
 }
 </script>
 
@@ -31,5 +46,18 @@ export default {
     border-bottom-color: white;
     border-bottom-width: 1px;
     border-bottom-style: solid;
+}
+
+.fade-enter {
+    opacity: 0;
+}
+
+.fade-enter-active {
+    transition: opacity 1s ease-out;
+}
+
+.fade-leave-enter {
+    transition: opacity .6s ease-out;
+    opacity: 0;
 }
 </style>
